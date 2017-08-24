6e0cf59b4523494be62c83c8765636cc72a04334
Nils Deppe <nd357@cornell.edu>
2017-08-24 16:43:14 -0400

    Make SetupCxxVersion take version as input

--- cmake/SetupCxxVersion.cmake
+++ cmake/SetupCxxVersion.cmake
@@ -1,10 +1,16 @@
 
-macro(use_cxx11)
-  if (CMAKE_VERSION VERSION_LESS "3.1")
-    set (CMAKE_CXX_FLAGS "-std=c++11 ${CMAKE_CXX_FLAGS}")
-  else ()
-    set (CMAKE_CXX_STANDARD 11)
-  endif ()
-endmacro(use_cxx11)
+option(CXX_VERSION "Use either C++11 (default) or C++14" OFF)
 
-use_cxx11()
+if(NOT ${CXX_VERSION})
+  set(CXX_VERSION "11")
+endif()
+
+macro(use_cxx_version)
+  if(CMAKE_VERSION VERSION_LESS "3.1")
+    set (CMAKE_CXX_FLAGS "-std=c++${CXX_VERSION} ${CMAKE_CXX_FLAGS}")
+  else()
+    set(CMAKE_CXX_STANDARD ${CXX_VERSION})
+  endif()
+endmacro(use_cxx_version)
+
+use_cxx_version()
